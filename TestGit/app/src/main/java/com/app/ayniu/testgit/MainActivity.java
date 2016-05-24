68d9229ad9b80b903c22483934b07039f5c8f4b9
Ayniu <Ayniu161@163.com>
2016-05-24 13:37:12 +0800

    测试在AS改动代码后，是否可同步到GitHub

--- TestGit/app/src/main/java/com/app/ayniu/testgit/MainActivity.java
+++ TestGit/app/src/main/java/com/app/ayniu/testgit/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //测试在AS改动代码，可否同步到GitHub（2016.05.24--Ayniu--）
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
